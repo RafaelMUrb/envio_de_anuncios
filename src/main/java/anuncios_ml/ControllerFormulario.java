86ece93d8dcb91d7a34ea78ea0c69c3b35b1f714
Rafael Urbano <rafezin2121@gmail.com>
2025-08-09 01:20:56 -0300

    Primeiro Commit

---
+++ src/main/java/anuncios_ml/ControllerFormulario.java
@@ -0,0 +1,14 @@
+package anuncios_ml;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ControllerFormulario {
+
+    @GetMapping("/home")
+    public String home() {
+      return "contentHome";
+    }
+}
