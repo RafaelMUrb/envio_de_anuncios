e9e137bcf275eac858c6217536563a7728d7e3d9
Rafael Moreira Urbano <rafaelurbano038@gmail.com>
2025-08-09 11:53:25 -0300

    atualizacao de rotas

--- src/main/java/anuncios_ml/ControllerFormulario.java
+++ src/main/java/anuncios_ml/ControllerFormulario.java
@@ -1,5 +1,6 @@
 package anuncios_ml;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,5 +11,10 @@ public class ControllerFormulario {
     @GetMapping("/home")
     public String home() {
       return "contentHome";
+    }
+
+    @RequestMapping("/register")
+    public ModelAndView productRegister(){
+        return new ModelAndView("productRegister");
     }
 }
